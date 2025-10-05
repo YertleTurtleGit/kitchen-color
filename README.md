f50aa3284f528a50ac55834b652638f2883301de
YertleTurtleGit <github@galzone.de>
2025-10-05 18:07:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Citations by h-Index
+
+*Do Scientists Directly or Indirectly Cite other Scientists by Reputation?*
+
+https://yertleturtlegit.github.io/citations-by-hindex/
---
+++ main.js
@@ -0,0 +1,85 @@
+/* global THREE */
+
+const canvas = /** @type {HTMLCanvasElement} */ (
+  document.getElementById("canvas")
+);
+canvas.width = 1920;
+canvas.height = 1440;
+
+const colorPicker = /** @type {HTMLInputElement} */ (
+  document.getElementById("colorPicker")
+);
+
+const camera = new THREE.OrthographicCamera();
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setClearColor(new THREE.Color("white"));
+
+const vertexShaderSource = /*glsl*/ `#version 300 es
+in vec3 position;
+in vec2 uv;
+
+out vec2 vUv;
+
+void main() {
+  gl_Position = vec4(position, 1.0);
+  vUv = uv;
+}
+`;
+
+const fragmentShaderSource = /*glsl*/ `#version 300 es
+precision highp float;
+
+in vec2 vUv;
+
+uniform sampler2D uImage;
+uniform sampler2D uMask;
+
+out vec4 fragmentColor;
+
+uniform vec3 wallColor;
+
+void main() {
+  vec3 imageColor = texture(uImage, vUv).rgb;
+  float maskColor = texture(uMask, vUv).r;
+
+  if (maskColor > 0.5) {
+    imageColor *= wallColor;
+  }
+
+
+  fragmentColor = vec4(imageColor, 1.0);
+}
+`;
+
+const textureLoader = new THREE.TextureLoader();
+const imageTexture = textureLoader.load("data/image.jpg");
+const maskTexture = textureLoader.load("data/mask.jpg");
+
+const material = new THREE.RawShaderMaterial({
+  vertexShader: vertexShaderSource,
+  fragmentShader: fragmentShaderSource,
+  uniforms: {
+    uImage: { value: imageTexture },
+    uMask: { value: maskTexture },
+    wallColor: { value: new THREE.Color(colorPicker.value) },
+  },
+});
+
+// Create a plane geometry
+const geometry = new THREE.PlaneGeometry(2, 2);
+const mesh = new THREE.Mesh(geometry, material);
+
+// Create scene and add mesh
+const scene = new THREE.Scene();
+scene.add(mesh);
+
+// Render function
+function render() {
+  renderer.render(scene, camera);
+}
+
+colorPicker.addEventListener("input", () => {
+  material.uniforms.wallColor.value = new THREE.Color(colorPicker.value);
+  render()
+});
+setTimeout(render, 500);
